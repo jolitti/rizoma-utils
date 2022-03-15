0f2795db55c8278686872a68e2a5134ceadc1ab3
jolitti <marcodeltrionfo@gmail.com>
2022-03-15 15:30:16 +0100

    Added cuboid subtraction, tests pass

--- src/rizomath/cuboid.py
+++ src/rizomath/cuboid.py
@@ -1,5 +1,6 @@
 from .interval import Interval, str_to_iv
 from dataclasses import dataclass
+from itertools import product
 from math import prod
 
 @dataclass(frozen=True)
@@ -11,16 +12,32 @@ class Cuboid:
         return prod((x.get_length() for x in self.points))
     
     def dims(self) -> int:
+        """Number of dimensions of the cuboid"""
         return len(self.points)
+
+    def __eq__(self,other:"Cuboid") -> bool:
+        if not isinstance(other,Cuboid): return False
+        if other.dims() != self.dims(): return False
+        return all(x==y for (x,y) in zip(self.points,other.points))
 
     def __and__(self,other:"Cuboid") -> "Cuboid":
         """Return intersection of two cuboids of the same dimensionality"""
         if not isinstance(other,Cuboid): return None
         if other.dims() != self.dims(): return None
-        #TODO - Create a __and__ overload for Interval
         intersection = [a&b for (a,b) in zip(self.points,other.points)]
         if None in intersection: return None
         return Cuboid(intersection)
+
+    def __sub__(self,other:"Cuboid") -> list["Cuboid"]:
+        """Return list of cuboids that add together to (self-other)"""
+        if not isinstance(other,Cuboid): return []
+        if not self.dims() == other.dims(): return []
+        intersection = self & other
+        if not intersection: return []
+        # List of list of segments of intervals in respective dimentions
+        sections = [[x&y] + (x-y)  for x,y in zip(self.points,other.points)]
+        result = [Cuboid(p,self.value) for p in product(*sections) if Cuboid(p)!=intersection]
+        return result
 
 def str_to_cb(s:str, val:any=None) -> Cuboid:
     """Converts a string of the type a..b c..e [...] into a Cuboid"""
--- tests/test_interval_cuboid.py
+++ tests/test_interval_cuboid.py
@@ -36,12 +36,30 @@ def test_interval_sub():
 
 def test_cubo():
     x = cb.str_to_cb("1..2 1..2 1..2")
+    x_equiv = cb.str_to_cb("1..2 1..2 1..2", 42)
     assert x.volume() == 8
+    assert x == x
+    assert x == x_equiv
     y = cb.str_to_cb("1..3 1..3", True)
     z = cb.str_to_cb("3..4 4..6 7..11")
     assert y.volume() == 9
     assert y.value is True
+    assert x.value is None
     assert x & y is None
     assert x & z is None
     x2 = cb.str_to_cb("1..1 1..1 1..1")
     assert (x & x2).volume() == 1
+
+def test_sub_cubo_sections():
+    """Ensure that the correct number of sub-cuboids is formed"""
+    x = cb.str_to_cb("1..3 1..3 1..3")
+    y = cb.str_to_cb("2..2 2..2 2..2")
+    minus = x - y
+    assert len(minus) == 9+9+8
+    assert sum(c.volume() for c in minus) == (x.volume() - y.volume())
+
+def test_sub_cubo_volumes():
+    x = cb.str_to_cb("1..100 1..1000 1..500")
+    y = cb.str_to_cb("50..200 400..10000 -900..242") 
+    diff_volume = sum(c.volume() for c in y-x)
+    assert diff_volume == y.volume() - (x & y).volume()
